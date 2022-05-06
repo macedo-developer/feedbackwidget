a3ea92f095805dfc841cbb8a6c9d256b12fe3209
Renata Macedo <macedorenata.developer@gmail.com>
2022-05-06 00:03:10 -0300

    Creating steps widget

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import bugImgUrl from "../../assets/bug.svg";
+import ideaImgUrl from "../../assets/idea.svg";
+import thoughtImgUrl from "../../assets/thought.svg";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+
+export const feedbacksTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImgUrl,
+      alt: "Imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImgUrl,
+      alt: "Imagem de uma lampada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImgUrl,
+      alt: "Imagem de uma nuvem de pensamento",
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbacksTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChange={setFeedbackType} />
+      ) : (
+        <FeedbackContentStep
+          feedbackType={feedbackType}
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
+      )}
+      <footer className="text-xs text-neutral-400">
+        Feito com ❤️ por
+        <a
+          className="underline underline-offeset-2"
+          href="https://renatamacedo.vercel.app"
+        >
+          Renata
+        </a>
+      </footer>
+    </div>
+  );
+}
